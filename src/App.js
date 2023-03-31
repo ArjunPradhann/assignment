81d04a836baabc62270ac3752ee7185bcd075296
ArjunPradhann <arjunpradhan46@gmail.com>
2023-03-31 09:13:59 +0530

    "next 2"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import LeftBar from "./components/LeftBar";
+import MainCompo from "./components/Main/MainCompo";
 
 function App() {
   return (
---
+++ src/components/Main/MainCompo.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+export default function MainCompo() {
+  return (
+    <div className="mt-3 border">
+      <div>
+        <h6>Post A Job</h6>
+        <label>
+          Job Title<span>*</span>
+        </label>
+        <br />
+        <input placeholder="Enter job title" type="text"></input>
+      </div>
+      <div>
+        <label>
+          Job Description<span>*</span>
+        </label>
+        <br />
+        <input type="text"></input>
+      </div>
+      <div>
+        <label>
+          Skiils Required<span>*</span>
+        </label>
+        <br />
+        <input placeholder="Enter skills required" type="text"></input>
+      </div>
+      <div className="d-flex flex-row">
+        <div>
+        <label for="cars">Industry Type<span>*</span></label>
+        <br />
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Select industry type</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        </div>
+        <div>
+        <label for="cars">Job Type<span>*</span></label>
+          <br />
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Select job type</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        </div>
+      </div>
+      <div className="d-flex flex-row">
+      <div>
+        <label for="cars">Work Type<span>*</span></label>
+        <br />
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Enter work rate</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        </div>
+        <div>
+        <label for="cars">Experience<span>*</span></label>
+        <br />
+        <input placeholder="Enter min exp"></input>
+        <input placeholder="Enter max exp"></input>
+        </div>
+      </div>
+    </div>
+  );
+}
